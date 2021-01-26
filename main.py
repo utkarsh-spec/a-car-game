281d699d0bee2f9afaeae53e2412c41acb409987
utkarsh <ayzfornothing@gmail.com>
2021-01-26 11:35:11 +0530

    continuous movement of player and background

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("a Car Game")
 move_right = [pygame.image.load('1.png'), pygame.image.load('2.png'), pygame.image.load('3.png')]
 move_left = [pygame.image.load('1.png'), pygame.image.load('2.png'), pygame.image.load('3.png')]
 bg = [pygame.image.load('background-1.png'), pygame.image.load('background-2.png'), pygame.image.load('background-3.png')]
-char = pygame.image.load('1.png')
+# char = pygame.image.load('1.png')
 
 clock = pygame.time.Clock()
 
@@ -20,30 +20,30 @@ class Player(object):
         self.width = width
         self.height = height
         self.vel = 20
-        self.left = False
-        self.right = False
+        self.car_move = True  # replace left with car_move and deleted right
+        self.bg_move = True
         self.run_count = 0
+        self.bg_count = 0
 
 # player movement
     def move(self, display):
-        if self.run_count + 1 >= 9:
-            self.run_count = 0
 
-        if self.left:
-            display.blit(move_left[self.run_count // 3], (self.x, self.y))
-            self.run_count += 1
-        elif self.right:
-            display.blit(move_right[self.run_count // 3], (self.x, self.y))
-            self.run_count += 1
-        else:
-            display.blit(char, (self.x, self.y))
+        if self.bg_count + 1 >= 9:
+            self.bg_count = 0
+            
+        if self.bg_move:
+            display.blit(bg[self.bg_count // 3], (0, 0))
+            self.bg_count += 1
+
+        if self.car_move:
+            display.blit(move_left[self.bg_count // 3], (self.x, self.y))
+            self.bg_count += 1
 
 
 car = Player(225, 350, 10, 30)
 
 
 def game_window():
-    display.blit(bg[car.run_count // 3], (0, 0))
     car.move(display)
     pygame.display.update()
 
@@ -51,7 +51,7 @@ def game_window():
 run = True
 # main window
 while run:
-    clock.tick(27)
+    clock.tick(18)
 # closing window loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,16 +62,10 @@ while run:
     if keys[pygame.K_LEFT] and car.x > car.vel:
         if car.x > 100:
             car.x -= car.vel
-            car.left = True
-            car.right = False
     # right arrow key input
     elif keys[pygame.K_RIGHT] and car.x < 500 - car.width - car.vel:
         car.x += car.vel
-        car.left = False
-        car.right = True
     else:
-        car.left = False
-        car.right = False
         car.run_count = 0
 
     game_window()
