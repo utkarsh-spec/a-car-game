c274b7edee0776063013888aff4b40bac2e90282
utkarsh <ayzfornothing@gmail.com>
2021-02-07 22:52:44 +0530

    added healthabr collision and score

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ bg = [pygame.image.load('background-1.png'), pygame.image.load('background-2.png
 enemy = pygame.image.load('Car.png')
 
 clock = pygame.time.Clock()
+score = 0
 
 
 class Player(object):
@@ -23,6 +24,7 @@ class Player(object):
         self.car_move = True
         self.bg_move = True
         self.bg_count = 0
+        self.hitbox = (self.x, self.y, 100, 216)
 
 # player movement
     def move(self, display):
@@ -37,6 +39,8 @@ class Player(object):
         if self.car_move:
             display.blit(move_left[self.bg_count // 3], (self.x, self.y))
             self.bg_count += 1
+        self.hitbox = (self.x, self.y, 100, 216)
+        pygame.draw.rect(display, (225, 0, 0), self.hitbox, 2)
 
 
 class Projectile(object):
@@ -58,18 +62,40 @@ class Enemy(object):
         self.x = x
         self.y = y
         self.vel = 3
+        self.hitbox = (self.x, self.y, 91, 211)
+        self.health = 10
+        self.visible = True
 
     def draw(self, display):
-        display.blit(enemy, (self.x, self.y))
         self.move()
+        if self.visible:
+            display.blit(enemy, (self.x, self.y))
+
+            pygame.draw.rect(display, (255, 0, 0), (self.hitbox[0], self.hitbox[1]-20, 50, 10))
+            pygame.draw.rect(display, (0, 255, 0), (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10))
+            self.hitbox = (self.x, self.y, 91, 211)
+            pygame.draw.rect(display, (225, 0, 0), self.hitbox, 2)
 
     def move(self):
         if self.y < 600:
             self.y += self.vel
             if self.y >= 600:
+                self.visible = True
+                self.health = 10
                 self.y = -200
                 if self.vel <= 10:
                     self.vel += 1
+
+    def hit(self):
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
+            # self.health = 10
+            global score
+            score += 1
+
+        print('hit')
 
 
 car1 = True
@@ -103,15 +129,20 @@ def effect(display):
 # main loop
 def game_window():
     car.move(display)
+    text = font.render('Score: ' + str(score), 1, (0, 0, 0))
+    display.blit(text, (500, 10))
     effect(display)
 
     for bullet in bullets:
         bullet.draw(display)
+
+    # update
     pygame.display.update()
 
 
 run = True
 bullets = []
+font = pygame.font.SysFont('comicsans', 30, True)
 # object generator
 car = Player(225, 350, 100, 216)
 enm = Enemy(100, -350)
@@ -126,6 +157,30 @@ while run:
             run = False
 
     for bullet in bullets:
+        if bullet.y - bullet.radius < enm.hitbox[1] + enm.hitbox[3] and bullet.y + bullet.radius > enm.hitbox[1]:
+            if bullet.x + bullet.radius > enm.hitbox[0] and bullet.x - bullet.radius < enm.hitbox[0] + enm.hitbox[2]:
+                enm.hit()
+                if enm.visible:
+                    bullets.pop(bullets.index(bullet))
+
+        if bullet.y - bullet.radius < enm2.hitbox[1] + enm2.hitbox[3] and bullet.y + bullet.radius > enm2.hitbox[1]:
+            if bullet.x + bullet.radius > enm2.hitbox[0] and bullet.x - bullet.radius < enm2.hitbox[0] + enm2.hitbox[2]:
+                enm2.hit()
+                if enm2.visible:
+                    bullets.pop(bullets.index(bullet))
+
+        if bullet.y - bullet.radius < enm3.hitbox[1] + enm3.hitbox[3] and bullet.y + bullet.radius > enm3.hitbox[1]:
+            if bullet.x + bullet.radius > enm3.hitbox[0] and bullet.x - bullet.radius < enm3.hitbox[0] + enm3.hitbox[2]:
+                enm3.hit()
+                if enm3.visible:
+                    bullets.pop(bullets.index(bullet))
+
+        if bullet.y - bullet.radius < enm4.hitbox[1] + enm4.hitbox[3] and bullet.y + bullet.radius > enm4.hitbox[1]:
+            if bullet.x + bullet.radius > enm4.hitbox[0] and bullet.x - bullet.radius < enm4.hitbox[0] + enm4.hitbox[2]:
+                enm4.hit()
+                if enm4.visible:
+                    bullets.pop(bullets.index(bullet))
+
         if bullet.x < 650 and bullet.x > 0:
             bullet.y -= bullet.vel
         else:
